cae3d375cb4f23d75b97bedfea670cf9c3b4d7b3
Cytosine <1316264211@qq.com>
2017-03-08 17:51:20 +0800

    2-6

---
+++ C/2-5/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    br();
+    printf(", ");
+    ic();
+    printf("\n");
+    ic();
+    printf(",\n");
+    br();
+
+    return 0;
+}
+
+void br(){
+    printf("Brazil, Russia");
+}
+
+void ic(){
+    printf("India, China");
+}
---
+++ C/2-6/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int toes=10;
+    int twiceToes=toes*2;
+    int squareToes=toes*toes;
+
+    printf("The toes is %d,twice toes is %d and the toes squared is %d.\n",toes,twiceToes,squareToes);
+
+    return 0;
+}
