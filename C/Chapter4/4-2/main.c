2f060b89abf379c3edac50fffb5787f8176a0366
Cytosine <1316264211@qq.com>
2017-03-10 23:00:48 +0800

    4-2 finish

--- C/Chapter4/4-2/main.c
+++ C/Chapter4/4-2/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define charMax 30
 
 int main()
 {
-    printf("Hello world!\n");
+    char first[charMax];
+    printf("Please enter your first name:\n");
+    scanf("%s",&first);
+
+    printf("\"%s\"\n",first);
+    printf("\"%+20s\"\n",first);
+    printf("\"%-20s\"\n",first);
+    printf("\"%*s\"\n",strlen(first)+3,first);
+
     return 0;
 }
