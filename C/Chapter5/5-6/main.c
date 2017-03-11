82795bb486afb85a569fd98d6474b50ea350e6b6
Cytosine <1316264211@qq.com>
2017-03-11 18:05:17 +0800

    5-6 finished

---
+++ C/Chapter5/5-6/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int count=0,sum=0,max;
+
+    printf("How many numbers do you want to calculate?\n");
+    scanf("%d",&max);
+
+    while(count++ <max){
+        sum+=count*count;
+    }
+
+    printf("sum=%d\n",sum);
+
+    return 0;
+}
