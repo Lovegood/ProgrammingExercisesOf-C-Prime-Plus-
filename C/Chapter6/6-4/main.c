f8ac593d3202d69889d6f5db89914c6557de18d4
Cytosine <1316264211@qq.com>
2017-03-12 21:55:15 +0800

    6-4 finished

--- C/Chapter6/6-4/main.c
+++ C/Chapter6/6-4/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ASCII_A 65
+#define line 6
 
 int main()
 {
-    printf("Hello world!\n");
+    int count=0;
+    for(int i=1;i<=line;i++){
+        for(int j=0;j<i;j++){
+            printf("%c",ASCII_A+count);
+            count++;
+        }
+        printf("\n");
+    }
     return 0;
 }
