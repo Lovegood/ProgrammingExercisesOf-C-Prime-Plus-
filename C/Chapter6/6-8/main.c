1760e30bbf2ee784c6dd073dacb4b5bfc9cb3edf
Cytosine <1316264211@qq.com>
2017-03-25 19:27:53 +0800

    6-8 finished

--- C/Chapter6/6-8/main.c
+++ C/Chapter6/6-8/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    float f1,f2,result;
+
+    printf("Please enter two float-point numbers and separated them by space.\n");
+    while(scanf("%f %f",&f1,&f2)==2){
+        printf("(%f-%f)/(%f*%f)=%f\n",f1,f2,f1,f2,(f1-f2)/(f1*f2));
+        printf("Please enter two float-point numbers to continue or enter \'q\' to quit.\n");
+    }
+
     return 0;
 }
