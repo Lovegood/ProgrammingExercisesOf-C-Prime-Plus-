c11e7aa8a7a5a98e9148bc1cf0b03a71ca97c058
Cytosine <1316264211@qq.com>
2017-03-25 19:11:09 +0800

    6-7 finished

--- C/Chapter6/6-7/main.c
+++ C/Chapter6/6-7/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#define SIZE 50
 
 int main()
 {
-    printf("Hello world!\n");
+    char userInput[SIZE];
+
+    printf("Please enter a single word.\n");
+    scanf("%s",&userInput);
+
+    for(int i=strlen(userInput)-1;i>=0;i--){
+        printf("%c",userInput[i]);
+    }
+
     return 0;
 }
