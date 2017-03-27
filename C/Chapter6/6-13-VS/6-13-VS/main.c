e77e445700813069cce566f1b0b9e40f8f907e70
Cytosine <1316264211@qq.com>
2017-03-27 23:15:49 +0800

    6-13 finished

---
+++ C/Chapter6/6-13-VS/6-13-VS/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define SIZE 8
+
+int main()
+{
+	int num[SIZE];
+
+	for (int i = 0; i<SIZE; i++) {
+		num[i] = pow(2, i + 1);
+	}
+
+	int count = 0;
+	do {
+		printf("%d ", num[count]);
+		count++;
+	} while (count<SIZE);
+
+	system("pause");
+	return 0;
+}
