b3d42e9bfaeab091734c55142c9abd118a988342
Cytosine <1316264211@qq.com>
2017-03-27 22:33:16 +0800

    6-11 finished

--- C/Chapter6/6-11/main.c
+++ C/Chapter6/6-11/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#define SIZE 8
 
 int main()
 {
+    int num[SIZE];
+
+    for(int count=0;count<SIZE;count++){
+        printf("Please enter a integer:");
+        scanf("%d",&num[count]);
+    }
+
+    printf("Eight integers was read.Now I will print them in reverse order.\n");
+
+    for(int i=SIZE-1;i>=0;i--){
+        printf("%d ",num[i]);
+    }
+
     return 0;
 }
