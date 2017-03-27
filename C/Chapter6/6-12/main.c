81113e188ca5ce1df9cb202e55fa33351c7d484b
Cytosine <1316264211@qq.com>
2017-03-27 22:57:54 +0800

    6-12 finished

--- C/Chapter6/6-12/main.c
+++ C/Chapter6/6-12/main.c
@@ -3,6 +3,33 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int limit;
+    double sum;
+
+    while(1){
+        printf("limit of number of terms:");
+        scanf("%d",&limit);
+
+        if(limit<=0){
+            break;
+        }
+
+        //first
+        sum=0;
+        for(int i=1;i<=limit;i++){
+            sum+=1.0/i;
+        }
+        printf("%lf ",sum);
+
+        //second
+        sum=0;
+        int sign=1;
+        for(int i=1;i<=limit;i++){
+            sum+=(1.0/i)*sign;
+            sign=-sign;
+        }
+        printf("%lf\n",sum);
+    }
+
     return 0;
 }
