df4eb620a14a3bed0b2a03624fd925e526a74de8
Cytosine <1316264211@qq.com>
2017-03-27 22:25:24 +0800

    6-10

--- C/Chapter6/6-10/main.c
+++ C/Chapter6/6-10/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int lower,upper,result;
+
+    while(1){
+        printf("Enter lower and upper integer limits:");
+
+        if((scanf("%d %d",&lower,&upper)!=2)||lower>=upper){
+            break;
+        }
+
+        result=0;
+        for(int i=lower;i<=upper;i++){
+            result+=i*i;
+        }
+        printf("The sums of the squares from %d to %d is %d\n",lower*lower,upper*upper,result);
+    }
+
+    printf("Done");
+
     return 0;
 }
